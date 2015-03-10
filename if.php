c6b6ca8e89bb4622196bbc043eab206cb0d1d053
Jamie Haskell <jmehaskell@gmail.com>
2015-03-10 10:52:59 -0500

    more practice with intro PHP

--- if.php
+++ if.php
@@ -44,4 +44,40 @@
      echo "$b is not identical to $c\n";
  }
 
+
+
+
+ // Shorten the first 2 statements into an if/else
+ if ($a < $b) {
+     echo "$a is less than $b\n";
+ } else {
+     echo "$b is greater than $a\n";
+ }
+
+ // Shorten the second 2 statements into an if/else
+ if ($b >= $c) {
+     // output the appropriate result
+     echo "$b is greater than or equal to $c\n";
+ } else {
+     // output the appropriate result
+     echo "$b is less than $c\n";
+ }
+
+ // combine the last 4 conditionals into
+ // one if/else/elseif block that checks in order for:
+ // identical, equal, not identical, not equal
+ if ($b === $c) {
+     // output the appropriate result
+     echo "$b is identical to $c\n";
+ } else if ($b == $c) {
+     // output the appropriate result
+     echo "$b is equal to $c\n";
+ } else if ($b !== $c) {
+     // output the appropriate result
+     echo "$b is not identical to $c\n";
+ } else {
+     // output the appropriate result
+     echo "$b is not equal to $c\n";
+ }
+
  ?>

b88ddb3d57f9a6cd4fdb46e4c9d9ce698b3f1758
Jamie Haskell <jmehaskell@gmail.com>
2015-03-12 13:01:20 -0500

    refactored arithmetic php functions

--- arithmetic.php
+++ arithmetic.php
@@ -86,9 +86,66 @@
 
 
 //Verified versions that call an error function
+// function add($a, $b) {
+// 	if(is_numeric($a) && is_numeric($b)){
+//     	echo $a + $b . PHP_EOL;
+//     } else {
+//     	error($a, $b);
+//     }
+// }
+
+// function subtract($a, $b) {
+//     if(is_numeric($a) && is_numeric($b)){
+//     	echo $a - $b . PHP_EOL;
+//     } else {
+//     	error($a, $b);
+//     }
+// }
+
+// function multiply($a, $b) {
+// 	if(is_numeric($a) && is_numeric($b)){
+//     	echo $a * $b . PHP_EOL;
+//     } else {
+//     	error($a, $b);
+//     }
+// }
+
+// function divide($a, $b) {
+// 	if(is_numeric($a) && is_numeric($b) && $b != 0){
+//     	echo $a / $b . PHP_EOL;
+//     } else {
+//     	error($a, $b);
+//     }
+// }
+
+// function modulus($a, $b) {
+// 	if(is_numeric($a) && is_numeric($b) && $b != 0){
+//     	echo $a % $b . PHP_EOL;
+//     } else {
+//     	error($a, $b);
+//     }
+// }
+
+// function error($a, $b){
+// 	if($b == 0){
+//     	echo "Warning: cannot divide by zero: ";
+//     } else {
+//     	echo "ERROR: Both arguments must be numbers\n";
+//     }
+// }
+
+// add(3, 9);
+// subtract(3, 9);
+// multiply(3, 9);
+// divide(3, 0);
+// modulus(3, 0);
+// add(array(1,2,3), true);
+
+
+// Refactored functions that return results, rather than explicitly echoing them
 function add($a, $b) {
 	if(is_numeric($a) && is_numeric($b)){
-    	echo $a + $b . PHP_EOL;
+    	return $a + $b;
     } else {
     	error($a, $b);
     }
@@ -96,7 +153,7 @@ function add($a, $b) {
 
 function subtract($a, $b) {
     if(is_numeric($a) && is_numeric($b)){
-    	echo $a - $b . PHP_EOL;
+    	return $a - $b;
     } else {
     	error($a, $b);
     }
@@ -104,7 +161,7 @@ function subtract($a, $b) {
 
 function multiply($a, $b) {
 	if(is_numeric($a) && is_numeric($b)){
-    	echo $a * $b . PHP_EOL;
+    	return $a * $b;
     } else {
     	error($a, $b);
     }
@@ -112,7 +169,7 @@ function multiply($a, $b) {
 
 function divide($a, $b) {
 	if(is_numeric($a) && is_numeric($b) && $b != 0){
-    	echo $a / $b . PHP_EOL;
+    	return $a / $b;
     } else {
     	error($a, $b);
     }
@@ -120,7 +177,7 @@ function divide($a, $b) {
 
 function modulus($a, $b) {
 	if(is_numeric($a) && is_numeric($b) && $b != 0){
-    	echo $a % $b . PHP_EOL;
+    	return $a % $b;
     } else {
     	error($a, $b);
     }
@@ -128,18 +185,23 @@ function modulus($a, $b) {
 
 function error($a, $b){
 	if($b == 0){
-    	echo "Warning: cannot divide by zero: ";
-    	echo "$a % $b\n";
+    	echo "Warning: cannot divide by zero";
     } else {
     	echo "ERROR: Both arguments must be numbers\n";
     }
 }
 
-add(3, 9);
-subtract(3, 9);
-multiply(3, 9);
-divide(3, 0);
-modulus(3, 0);
-add(array(1,2,3), true);
+$sum = add(3, 9);
+echo "The sum of 3 and 9 is $sum\n";
+$difference = subtract(3, 9);
+echo "The difference between 3 and 9 is $difference\n";
+$product = multiply(3, 9);
+echo "The product of 3 and 9 is $product\n";
+echo "The quotient of 3 and 0 is $quotient\n";
+$quotient = divide(3, 0);
+echo "The remainder when 3 is divided by 0 is $remainder\n";
+$remainder = modulus(3, 0);
+echo "When you try to add an array with a boolean you get $summError\n";
+$summError = add(array(1,2,3), true);
 
 ?>

564a2782130f3ef2fbcda18c70a272e5ddf7c559
Jamie Haskell <jmehaskell@gmail.com>
2015-03-13 13:35:12 -0500

    refactored array comarison functions

--- search_arrays.php
+++ search_arrays.php
@@ -8,7 +8,7 @@
 	// Create a function that returns TRUE or FALSE if an array value is found. 
 	function search($person, $array){
 		$key = array_search($person, $array);
-		echo "\$key is $key\n";
+		// echo "\$key is $key\n";
 		//use is_numeric(), otherwise a key of zero will return false
 		if(is_numeric($key)){
 			// return 'TRUE';
@@ -26,11 +26,12 @@
 	// echo search('Mike', $otherNames).PHP_EOL;
 	// echo search('Amy', $otherNames).PHP_EOL;
 	
+	
 	// Create a function to compare 2 arrays that returns the number of values in common.
 	function comparison($array1, $array2){
 		$numInCommon = 0;
 		foreach($array1 as $person){
-			echo "{$person}'s ";
+			// echo "{$person}'s ";
 			$key = search("$person", $array2);
 			if(is_numeric($key)){
 				$numInCommon++;

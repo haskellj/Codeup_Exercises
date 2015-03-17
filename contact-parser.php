ba67b6ae2b06c35f7a2084543801be9bb8a46f20
Jamie Haskell <jmehaskell@gmail.com>
2015-03-17 17:26:13 -0500

    extra shuffles in blackjack and file parsing exercises

--- contact-parser.php
+++ contact-parser.php
@@ -7,11 +7,11 @@ function parseContacts($filename)
     // todo - read file and parse contacts
     $handle = fopen($filename, 'r');
     $contents = fread($handle, filesize($filename));
-    echo $contents;
+    // echo $contents;
     
     // Create an inital array of the contents by exploding based on the end of a line
     $contentsArray = explode("\n", $contents);
-    print_r($contentsArray);
+    // print_r($contentsArray);
 
     // Create smaller arrays within the large array by exploding each element at the pipes "|"
     // Delete old elements and replace them with the new small arrays
@@ -33,5 +33,24 @@ function parseContacts($filename)
 }
 
 var_dump(parseContacts('contacts.txt'));
+
+
+// Another option for changing the keys to strings and formatting the phone number
+foreach($contentsArray as $key => $innerArray){
+	$contact = explode("|", $innerArray);
+	$newArray[$key]['name'] = $contact[0];	// name
+
+	$contact[1] = substr($contact["number"], 0, 3)."-".substr($contact["number"], 3, 3)."-".substr($contact["number"], 6);	//format the phone number
+
+	$newArray[$key]['phone'] = $contact[1];	// phone number
+}
+
+
+
+
+
+
+
+
 
 
---
+++ report_parser.php
@@ -0,0 +1,99 @@
+<?php
+
+// Open, read, save the contents, then close the file
+	$filename = 'report.txt';
+	$handle = fopen($filename, 'r');
+	$contents = fread($handle, filesize($filename));
+	fclose($handle);
+
+// TODO: Output should be:
+// Ordered by sales units (high to low)
+// Columns should appear as: Sales Units | Full Name | Employee Number
+
+// Create a function that parses txt file into an array by exploding each line into an array element
+	$contentsArray = explode("\n", $contents);
+// Delete the line dividers
+	$equalsLineDivder = "===================================================";
+	$asteriskLineDivider = "***************************************************";
+	$oldHeader = "Employee Number, First Name, Last Name, Sales Units";
+	$dividers = array($equalsLineDivder, $asteriskLineDivider, $oldHeader);
+	$contentsArray = array_diff($contentsArray, $dividers);
+	// print_r($contentsArray);
+// Separate first 3 elements (to get rid of unnecessary top content) into their own array for later use
+	$reportHeaderArray = array_slice($contentsArray, 0, 3);
+	$contentsArray = array_slice($contentsArray, 3);
+	// print_r($reportHeaderArray);
+	// print_r($contentsArray);
+// foreach loop through each line and explode on the commas, creating smaller arrays containing 3 elements each
+// Change the numeric indices to strings by pushing onto a new empty array with predefined indices at each pass of the loop
+	foreach($contentsArray as $key => $eachEmployeeData){
+		$employeeStats = explode(', ', $eachEmployeeData);
+		$parentArray[$key]['Employee Number'] = $employeeStats[0];	// Renames the 0 index to 'Employee Numeber'
+		$parentArray[$key]['First Name'] = $employeeStats[1];	//Renames the 1 index to 'First Name'
+		$parentArray[$key]['Last Name'] = $employeeStats[2];	//Renames the 2 index to 'Last Name'
+		$parentArray[$key]['Unit Sales'] = $employeeStats[3];	//Renames the 3 index to 'Unit Sales'
+	}
+	// print_r($parentArray);
+
+
+// Then sort this new array by "Sales Units"
+	// The following function was found on PHP.net under the 'sort' documentation
+	function array_sort($array, $on, $order=SORT_ASC)
+	{
+	    $new_array = array();
+	    $sortable_array = array();
+
+	    if (count($array) > 0) {
+	        foreach ($array as $k => $v) {
+	            if (is_array($v)) {
+	                foreach ($v as $k2 => $v2) {
+	                    if ($k2 == $on) {
+	                        $sortable_array[$k] = $v2;
+	                    }
+	                }
+	            } else {
+	                $sortable_array[$k] = $v;
+	            }
+	        }
+
+	        switch ($order) {
+	            case SORT_ASC:
+	                asort($sortable_array);
+	            break;
+	            case SORT_DESC:
+	                arsort($sortable_array);
+	            break;
+	        }
+
+	        foreach ($sortable_array as $k => $v) {
+	            $new_array[$k] = $array[$k];
+	        }
+	    }
+
+	    return $new_array;
+	}
+
+	$sortedBySalesArray = array_sort($parentArray, 'Unit Sales', SORT_DESC); // Sort decending order: highest to lowest
+	// print_r($sortedBySalesArray); 
+
+
+// Echo the Header information
+	echo "$reportHeaderArray[2]	|	$reportHeaderArray[1]	|	$reportHeaderArray[0]".PHP_EOL;
+	echo "Sales Units 	|	Employee Name 	|	Employee Number".PHP_EOL;
+// Create a function that foreach loops through the new global array, and: 
+// echo "$Array[$key]['Sales Units'] 		|	$Array[$key]['First Name'] $Array[$key]['Last Name'] 		|	$Array[$key]['Employee Number']";
+	foreach($sortedBySalesArray as $employee){
+		echo $employee['Unit Sales']."	|	".$employee['First Name']." ".$employee['Last Name']."	|	".$employee['Employee Number'].PHP_EOL;
+		// $compactedEmployee = implode('	|	', $employee);
+		// $newArray[] = $compactedEmployee;
+	}
+
+
+
+
+
+
+
+
+
+

76c87441d956c4bfddf9e525d17bf97c146d549f
Jamie Haskell <jmehaskell@gmail.com>
2015-03-18 15:20:52 -0500

    writing to a file

---
+++ logger.php
@@ -0,0 +1,23 @@
+<?php
+
+function logMessage($logLevel, $message)
+{
+	date_default_timezone_set('America/Chicago');
+	$date = date('Y-m-d');
+    
+    $filename = "log {$date}.txt";
+    $handle = fopen($filename, 'a+');
+    fwrite($handle, PHP_EOL.date("Y-m-d H:i:s") . " [$logLevel] $message");
+    fclose($handle);
+}
+
+function logInfo($message){
+	logMessage("INFO", $message);
+}
+
+function logError($message){
+	logMessage("ERROR", $message);
+}
+
+logInfo("This is an informational message.");
+logError("An error has occurred.");

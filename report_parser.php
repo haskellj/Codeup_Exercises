a46308efd8cb11d70614ac21d80682fa3216e03e
Jamie Haskell <jmehaskell@gmail.com>
2015-03-18 11:34:34 -0500

    report formatter with functions

--- report_parser.php
+++ report_parser.php
@@ -2,42 +2,61 @@
 
 // Open, read, save the contents, then close the file
 	$filename = 'report.txt';
-	$handle = fopen($filename, 'r');
-	$contents = fread($handle, filesize($filename));
-	fclose($handle);
 
-// TODO: Output should be:
-// Ordered by sales units (high to low)
-// Columns should appear as: Sales Units | Full Name | Employee Number
-
-// Create a function that parses txt file into an array by exploding each line into an array element
-	$contentsArray = explode("\n", $contents);
-// Delete the line dividers
-	$equalsLineDivider = "===================================================";
-	$asteriskLineDivider = "***************************************************";
-	$oldHeader = "Employee Number, First Name, Last Name, Sales Units";
-	$dividers = array($equalsLineDivider, $asteriskLineDivider, $oldHeader);
-	$contentsArray = array_diff($contentsArray, $dividers);
-	// print_r($contentsArray);
-// Separate first 3 elements (to get rid of unnecessary top content) into their own array for later use
-	$reportHeaderArray = array_slice($contentsArray, 0, 3);
-	$contentsArray = array_slice($contentsArray, 3);
-	// print_r($reportHeaderArray);
-	// print_r($contentsArray);
-// foreach loop through each line and explode on the commas, creating smaller arrays containing 3 elements each
-// Change the numeric indices to strings by pushing onto a new empty array with predefined indices at each pass of the loop
-	foreach($contentsArray as $key => $eachEmployeeData){
-		$employeeStats = explode(', ', $eachEmployeeData);
-		$parentArray[$key]['Employee Number'] = $employeeStats[0];	// Renames the 0 index to 'Employee Numeber'
-		$parentArray[$key]['First Name'] = $employeeStats[1];	//Renames the 1 index to 'First Name'
-		$parentArray[$key]['Last Name'] = $employeeStats[2];	//Renames the 2 index to 'Last Name'
-		$parentArray[$key]['Unit Sales'] = $employeeStats[3];	//Renames the 3 index to 'Unit Sales'
+	function openReadFile($filename){
+		$handle = fopen($filename, 'r');
+		$contents = fread($handle, filesize($filename));
+		fclose($handle);
+		organize($contents);
+		// return $contents;
 	}
-	// print_r($parentArray);
+
+// TODO: Use the file report.txt to output its contents
+// Output should be ordered by sales units (high to low)
+// Columns should appear as: Sales Units | Full Name | Employee Number
+	
+	function organize($contents){
+	// Create a function that parses txt file into an array by exploding each line into an array element
+		$contentsArray = explode("\n", $contents);
+	// Delete the line dividers
+		$equalsLineDivider = "===================================================";
+		$asteriskLineDivider = "***************************************************";
+		$oldHeader = "Employee Number, First Name, Last Name, Sales Units";
+		$dividers = array($equalsLineDivider, $asteriskLineDivider, $oldHeader);
+		$contentsArray = array_diff($contentsArray, $dividers);
+		// print_r($contentsArray);
+	// Separate first 3 elements (to get rid of unnecessary top content) into their own array for later use
+		$reportHeaderArray = array_slice($contentsArray, 0, 3);
+		$contentsArray = array_slice($contentsArray, 3);
+		// print_r($reportHeaderArray);
+		// print_r($contentsArray);
+	// foreach loop through each line and explode on the commas, creating smaller arrays containing 3 elements each
+	// Change the numeric indices to strings by pushing onto a new empty array with predefined indices at each pass of the loop
+		foreach($contentsArray as $key => $eachEmployeeData){
+			$employeeStats = explode(', ', $eachEmployeeData);
+			$parentArray[$key]['Employee Number'] = $employeeStats[0];	// Renames the 0 index to 'Employee Numeber'
+			$parentArray[$key]['First Name'] = $employeeStats[1];	//Renames the 1 index to 'First Name'
+			$parentArray[$key]['Last Name'] = $employeeStats[2];	//Renames the 2 index to 'Last Name'
+			$parentArray[$key]['Unit Sales'] = $employeeStats[3];	//Renames the 3 index to 'Unit Sales'
+		}
+		// print_r($parentArray);
+		
+		// Echo the Header information
+		echo $asteriskLineDivider."**************************".PHP_EOL;
+		echo "$reportHeaderArray[2]	|	$reportHeaderArray[0]	|	$reportHeaderArray[1]".PHP_EOL;
+		echo $equalsLineDivider."==========================".PHP_EOL;
+		echo "Sales Units 	|	Employee Name 		|	Employee Number".PHP_EOL;
+		echo "_____________________________________________________________________________".PHP_EOL;
+
+		$sortedBySalesArray = array_sort($parentArray, 'Unit Sales', SORT_DESC); // Sort decending order: highest to lowest
+		// print_r($sortedBySalesArray); 
+		echoFormatted($sortedBySalesArray);
+		// return $sortedBySalesArray;
+	}
 
 
 // Then sort this new array by "Sales Units"
-	// The following function was found on PHP.net under the 'sort' documentation
+// The following function was found on PHP.net under the 'sort' documentation
 	function array_sort($array, $on, $order=SORT_ASC)
 	{
 	    $new_array = array();
@@ -73,33 +92,28 @@
 	    return $new_array;
 	}
 
-	$sortedBySalesArray = array_sort($parentArray, 'Unit Sales', SORT_DESC); // Sort decending order: highest to lowest
-	// print_r($sortedBySalesArray); 
-
-
-// Echo the Header information
-	echo $asteriskLineDivider."**************************".PHP_EOL;
-	echo "$reportHeaderArray[2]	|	$reportHeaderArray[1]	|	$reportHeaderArray[0]".PHP_EOL;
-	echo $equalsLineDivider."==========================".PHP_EOL;
-	echo "Sales Units 	|	Employee Name 		|	Employee Number".PHP_EOL;
-	echo "_____________________________________________________________________________".PHP_EOL;
 // Create a function that foreach loops through the new global array, and: 
 // echo "$Array[$key]['Sales Units'] 		|	$Array[$key]['First Name'] $Array[$key]['Last Name'] 		|	$Array[$key]['Employee Number']";
-	foreach($sortedBySalesArray as $employee){
-		// echo "\t".$employee['Unit Sales']."\t\t".$employee['First Name']." ".$employee['Last Name']."\t\t\t".$employee['Employee Number'].PHP_EOL;
-		$sales = $employee['Unit Sales'];
-		$name = $employee['First Name']." ".$employee['Last Name'];
-		$employeeNum = $employee['Employee Number'];
-		
-		// One formatting option:
-		// printf("%' 8d%' 32s%' 25u\n", $sales, $name, $employeeNum);
+	function echoFormatted($sortedBySalesArray){
+		foreach($sortedBySalesArray as $employee){
+			// echo "\t".$employee['Unit Sales']."\t\t".$employee['First Name']." ".$employee['Last Name']."\t\t\t".$employee['Employee Number'].PHP_EOL;
+			$sales = $employee['Unit Sales'];
+			$name = $employee['First Name']." ".$employee['Last Name'];
+			$employeeNum = $employee['Employee Number'];
+			
+			// One formatting option:
+			// printf("%' 8d%' 32s%' 25u\n", $sales, $name, $employeeNum);
 
-		// Nicer formatting option:
-		 $format = str_pad($sales, 15, " ", STR_PAD_BOTH);
-		 $format .= str_pad($name, 35, " ", STR_PAD_BOTH);
-		 $format .= str_pad($employeeNum, 25, " ", STR_PAD_BOTH).PHP_EOL;
-		 echo $format;
+			// Nicer formatting option:
+			 $format = str_pad($sales, 15, " ", STR_PAD_BOTH);
+			 $format .= str_pad($name, 35, " ", STR_PAD_BOTH);
+			 $format .= str_pad($employeeNum, 25, " ", STR_PAD_BOTH).PHP_EOL;
+			 echo $format;
+		}
 	}
+
+// Call initializing function
+	openReadFile($filename);
 
 
 

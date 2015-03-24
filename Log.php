4a032b511d89ce11d3da365d56cfa87cada5ffa6
Jamie Haskell <jmehaskell@gmail.com>
2015-03-24 16:13:44 -0500

    created Log class

--- Log.php
+++ Log.php
@@ -5,6 +5,12 @@
 
 		public function logMessage($logLevel, $message)
 		{
+		    // Set timezone and date format
+		    date_default_timezone_set('America/Chicago');
+			$date = date('Y-m-d');
+		    
+		    // Assign a file to the filename property inside the instance of the Log class
+			$this->filename = "log {$date}.log";
 		    $handle = fopen($this->filename, 'a+');
 		    fwrite($handle, PHP_EOL.date("Y-m-d H:i:s") . " [$logLevel] $message");
 		    fclose($handle);

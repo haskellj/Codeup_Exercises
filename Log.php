b33a636df86baf7eee34de014ac72064a047dd76
Jamie Haskell <jmehaskell@gmail.com>
2015-03-25 10:44:22 -0500

    added constructor and destructor to Log class

--- Log.php
+++ Log.php
@@ -2,18 +2,32 @@
 	class Log
 	{
 		public $filename; 
+		public $handle;
+
+		public function __construct($prefix)
+		{
+			// Set timezone and date format
+		    date_default_timezone_set('America/Chicago');
+			$date = date('Y-m-d');
+		    
+			if(!isset($prefix)){
+				$prefix = 'log';
+			}
+			$this->filename = "{$prefix}-{$date}.log";
+			$this->handle = fopen($this->filename, 'a+');
+		}
 
 		public function logMessage($logLevel, $message)
 		{
 		    // Set timezone and date format
-		    date_default_timezone_set('America/Chicago');
-			$date = date('Y-m-d');
+		    // date_default_timezone_set('America/Chicago');
+			// $date = date('Y-m-d');
 		    
 		    // Assign a file to the filename property inside the instance of the Log class
-			$this->filename = "log {$date}.log";
-		    $handle = fopen($this->filename, 'a+');
-		    fwrite($handle, PHP_EOL.date("Y-m-d H:i:s") . " [$logLevel] $message");
-		    fclose($handle);
+			// $this->filename = "log {$date}.log";
+		    // $handle = fopen($this->filename, 'a+');
+		    fwrite($this->handle, PHP_EOL.date("Y-m-d H:i:s") . " [$logLevel] $message");
+		    // fclose($handle);
 		}
 
 		public function logInfo($message)
@@ -24,6 +38,11 @@
 		public function logError($message)
 		{
 			$this->logMessage("ERROR", $message);
+		}
+
+		public function __destruct()
+		{
+			fclose($this->handle);
 		}
 	}
 ?>
--- log_test.php
+++ log_test.php
@@ -3,12 +3,19 @@
 	// link to the Log class
 	require_once 'Log.php';
 
+// Prior to __construct() and __destruct() being included in the Log class
 	// Create an instance of the Log class
-	$logEntry = new Log();
+	// $logEntry = new Log();
 
 	// Call the methods (a.k.a. functions within the class using this insance)
-	$logEntry->logInfo("This is a test of the logInfo function");
-	$logEntry->logError('This is a test of the logError function');
+	// $logEntry->logInfo("This is a test of the logInfo function");
+	// $logEntry->logError('This is a test of the logError function');
 
+// After __construc() and __destruct() are included in the Log class
+	// Create an instance of the Log class
+	$newEntry = new Log('client');
+	$newEntry->logInfo('This is a test of the logInfo() function along with the __construct() function');
+	$newEntry->logError('This is a test of the logError() function along with the __construct() function');
+	$newEntry->logInfo("Don't worry, the __destruct() function closed the file for us");
 ?>
 
